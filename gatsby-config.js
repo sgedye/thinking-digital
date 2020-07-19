d64a7349a6b9ddc16074a3995bdc2caf4f481a3c
Shaun Gedye <sgedye88@yahoo.com.au>
2020-07-19 15:52:31 +0800

    :package: Updated package.json | fixed styled-components issue :tada:

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,6 +21,7 @@ module.exports = {
     },
     `gatsby-plugin-sass`,
     `gatsby-plugin-sharp`,
-    `gatsby-transformer-sharp`
+    `gatsby-transformer-sharp`,
+    'gatsby-plugin-styled-components'
   ]
 };
